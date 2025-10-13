cda6b3b0d7458e406a32970903e9ae4e3ffb40b7
bhoomika1996 <bhoomikagajpal@gmail.com>
2025-10-13 20:10:18 +0530

    refactoring expenseTracker

--- expenseTracker/src/main/java/com/bg/expensetracker/repository/CategoryRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bg.expensetracker.repository;
-
-import com.bg.expensetracker.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo  extends JpaRepository<Category, Long> {
-    Category findByName(String name);
-}
--- expenseTracker/src/main/java/com/bg/expensetracker/repository/ExpenseRepo.java
+++
@@ -1,7 +0,0 @@
-package com.bg.expensetracker.repository;
-
-import com.bg.expensetracker.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepo extends JpaRepository<Expense, Long> {
-}
